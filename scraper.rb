c30de4d2e71d3abfdebf3bfd0b416c32a585c77a
Andrii Korzh <Andrii.Korzh@gmail.com>
2017-01-24 03:43:54 +0200

    update scraper

--- scraper.rb
+++ scraper.rb
@@ -3,33 +3,39 @@ require 'mechanize'
 
 agent = Mechanize.new
 
+id_re = /\/(\d+)-/
+site_url = "http://vvm-auto.ru"
+
 def parse_page(url, data={})
+  page = agent.get("#{site_url}/#{url}")
+
+  content = page.search('[itemprop="articleBody"]').first.to_s
+
+  id = id_re.match(url).to_s.to_i
   data.merge!({
-    id: 1,
-    type: 'experience',
-    title: 'Title',
-    annotation_image: 'Annotation image',
-    annotation_text: 'Annotation',
-    article: 'Full text',
+    id: id,
+    content: content,
     url: url
   }).to_s
 end
 
 {
-  experience: "http://vvm-auto.ru/opyt-ekspluatatsii",
-  articles: "http://vvm-auto.ru/publikatsii",
-  tests: "http://vvm-auto.ru/test-obzor"
-}.each do |type, base_url|
-  p "[debug] Load #{type} from #{base_url}"
-  page = agent.get(base_url)
+  experience: "opyt-ekspluatatsii",
+  articles: "publikatsii",
+  tests: "test-obzor"
+}.each do |type, url|
+  p "[debug] Load #{type} from #{url}"
+  page = agent.get("#{site_url}/#{url}")
   annotations = page.search('#content [itemprop="blogPost"]')
   annotations.each do |annotation|
-    url = annotation.search('a').first.attribute('href')
+    url = annotation.search('a').first.attribute('href').value
+
     title = annotation.search('a').first.child.content
     annotation_text = annotation.search('p').first.child.content
-    annotation_image = annotation.search('[itemprop="thumbnailUrl"]').first.attribute('src')
+    annotation_image = annotation.search('[itemprop="thumbnailUrl"]').first.attribute('src').value
     data = {
       title: title,
+      type: type,
       annotation_text: annotation_text,
       annotation_image: annotation_image
     }
