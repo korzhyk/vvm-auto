8dc6637e4c47a0790bdd685c88e45e06ddad1c67
Andrii Korzh <Andrii.Korzh@gmail.com>
2017-01-24 04:06:33 +0200

    update

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,12 @@ def parse_page(url, data={})
   agent = Mechanize.new
   page = agent.get("#{$site_url}#{url}")
 
-  content = page.search('[itemprop="articleBody"]').first.to_s
+  content = page.search('[itemprop="articleBody"]').first
+  content.search('div.custom').each { |d| d.remove }
+  content.children.each do |c|
+    c.remove if c.blank?
+    c.attributes.clear
+  end
 
   id = /\/(\d+)-/.match(url).to_s.to_i
   data.merge!({
@@ -28,8 +33,7 @@ end
   annotations = page.search('#content [itemprop="blogPost"]')
   annotations.each do |annotation|
     url = annotation.search('a').first.attribute('href').value
-
-    title = annotation.search('a').first.child.content
+    title = annotation.search('a').first.child.content.strip
     annotation_text = annotation.search('p').first.child.content
     annotation_image = annotation.search('[itemprop="thumbnailUrl"]').first.attribute('src').value
     data = {
